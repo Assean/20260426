b6fe82c28fca7dcfb6edb1e6e30bfce58ee38194
sean <y20120816s@gmail.com>
2026-04-26 11:39:37 +0800

    fetch-20260426_updata

---
+++ sql.php
@@ -0,0 +1,12 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=ptqs";
+//建立PDO物件
+$pdo=new PDO($dsn,'admin','1234');
+
+$geturl = $_GET["do"];
+switch($geturl){
+    case 'getusers' :
+        $users = $pdo->query("SELECT * FROM users")->fetchAll();
+        echo json_encode($users);
+        break;
+}
