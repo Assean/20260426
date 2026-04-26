4656633de543b41655ac7c5c0b15252d30b2b9e7
sean <y20120816s@gmail.com>
2026-04-26 14:18:47 +0800

    v7.7_SQL_UP

--- sql.php
+++ sql.php
@@ -4,13 +4,17 @@ $dsn="mysql:host=localhost;charset=utf8;dbname=20260426";
 $pdo=new PDO($dsn,'admin','1234');
 
 $geturl = $_GET["do"];
-switch($geturl){
-    case 'getusers' :
-        $users = $pdo->query("SELECT * FROM users")->fetchAll();
+switch ($geturl){
+    case 'getusers':
+        $users = $pdo->query("select * from users")->fetchAll();
+        // print_r($users);
         echo json_encode($users);
         break;
-        case "edituser":
-            $user = $pdo->query("
-            UPDATE user set username={$_POST['username']},password={$_POST['password']}
-            ")
+    case "edituser":
+        $user = $pdo->query("
+        update users set username='{$_POST['username']}',password='{$_POST['password']}',role='{$_POST['role']}'
+        where id = {$_POST['id']}
+        ");
+        
+        break;
 }
